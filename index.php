24e6c6f942b4444645adc46c2affffdc47204efa
Dan Padilha <dan@ripenetwork.com>
2011-02-14 00:28:28 +1100

    Fix next chapter showing up on last chapter.

--- index.php
+++ index.php
@@ -143,12 +143,12 @@ if (isset($_GET['link'])){
         if ($number > 0){
             $layout = str_replace("{{PREVIOUS_CHAPTER}}", "<a id='prevchap' href='/chapter/".$chapters[$number-1]."'>&laquo; ".$chapter_list[$chapters[$number-1]]."</a>", $layout);
         } else {
-            $layout = str_replace("{{PREVIOUS_CHAPTER}}", "", $layout);
+            $layout = str_replace("{{PREVIOUS_CHAPTER}}", "<a id='prevchap'></a>", $layout);
         }
-        if ($number < sizeof($chapters)-1){
+        if ($number < sizeof($chapters)){
             $layout = str_replace("{{NEXT_CHAPTER}}", "<a id='nextchap' href='/chapter/".$chapters[$number+1]."'>".$chapter_list[$chapters[$number+1]]." &raquo;</a>", $layout);
         } else {
-            $layout = str_replace("{{NEXT_CHAPTER}}", "", $layout);
+            $layout = str_replace("{{NEXT_CHAPTER}}", "<a id='nextchap'></a>", $layout);
         }
 
         // Grab the list of sections in the chapter
