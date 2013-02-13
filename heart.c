8e9e43a871b52565a2969ab700fc613f9a270c5e
Dan Padilha <dan@ripenetwork.com>
2013-02-13 17:15:01 +1100

    Made some changes, added FAQs, improved intro.

--- heart.c
+++ heart.c
@@ -1,44 +1,49 @@
 #include <stdio.h>
 
-#define HT 25
-#define WD 50
+// Definitions
+#define HT 25 // Height of the shape
+#define WD 50 // Width of the shape
 
+// Function prototypes
 float power(float base, int index);
 
+// Allocates a 2-dimensional array grid for storing points of a function,
+// then prints the grid nicely.
+// Grid size and function are hard-coded in. Accepting input is left as
+// an exercise to the reader. Suckers.
 int main(int argc, char *argv[]) {
 
-    int grid[WD][HT]; //the grid that will store the shape
+    int   grid[WD][HT]; // The grid that will store the shape
+    int   i, j;         // Counters
+    float x, y;         // For storing x and y values of points
 
-    int i, j; //counters
-
-    float x, y; //for storing x and y values of points
-
-
-    //store the heart shape as a grid of 1s and 0s
-    for (i = 0;i<HT;i++) {
+    // Store the heart shape as a grid of 1s and 0s
+    for (i = 0; i < HT; i++) {
         
-        y = (i - HT/2)/(HT/3.0);
+        // Define the y-value for the heart function.
+        y = (i - HT/2) / (HT/3.0);
 
-        for (j = 0;j<WD;j++) {
+        for (j = 0; j < WD; j++) {
             
-            x = (j - WD/2)/(WD/2.5);
+            // Define the x-value for the heart function.
+            x = (j - WD/2) / (WD/2.5);
             
-            //check if a the point lies in the heart shape
+            // Check if a the point lies in the heart function.
             if (power((power(x, 2) + power(y, 2) - 1), 3) - 
                 (power(x, 2)*power(y, 3)) <= 0) {
-
-                //if so store a 1
+                // If so store a 1
                 grid[j][HT-i-1] = 1;
             } else {
-                //otherwise store a 0
+                // Otherwise store a 0
                 grid[j][HT-i-1] = 0;
             }
         }
     }
 
-    //print a heart of hashes
-    for (i = 0;i<HT;i++) {
-        for (j = 0;j<WD;j++) {
+    // Print out the grid with 1s replaced by hashes and
+    // 0s replaced by spaces.
+    for (i = 0; i < HT; i++) {
+        for (j = 0; j < WD; j++) {
             if (grid[j][i] == 1) {
                 printf("#");
             } else {
@@ -48,15 +53,15 @@ int main(int argc, char *argv[]) {
         printf("\n");
     }
 
-
     return 0;
 }
 
+// Raise a number to a given power.
 float power(float base, int index) {
     float answer = 1;
     int i;
 
-    for (i = 0;i<index;i++) {
+    for (i = 0; i < index; i++) {
         answer *= base;
     }
 
